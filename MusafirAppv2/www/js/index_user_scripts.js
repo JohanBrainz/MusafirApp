aa3b9c8d13b6b178b0b6f38d35a5b7ac0386cc4a
JohanBrainz <johannbrainz@gmail.com>
2014-09-10 16:44:55 +0800

    MusafirAppv2
    Added files for MusafirAppv2 in repo.

---
+++ MusafirAppv2/www/js/index_user_scripts.js
@@ -0,0 +1,14 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+         
+}
+ $(document).ready(register_event_handlers);
+})();
